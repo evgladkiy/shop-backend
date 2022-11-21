baa194454aafd01ac127ffb444a1ba6238fb4cee
Yauheni.Hladki <yauheni_hladki@epam.com>
2022-11-21 21:50:42 +0100

    task-7: move authorizer creation to import lambda

--- import-service/src/functions/importProductsFile/index.ts
+++ import-service/src/functions/importProductsFile/index.ts
@@ -1,4 +1,5 @@
 import { handlerPath } from '@libs/handler-resolver';
+import { AUTH_LAMBDA_NAME } from 'src/constants';
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
@@ -7,7 +8,14 @@ export default {
       http: {
         method: 'get',
         path: 'import',
-        cors: true
+        cors: true,
+        authorizer: {
+          name: 'basicAuth',
+          arn: 'arn:aws:lambda:${aws:region}:${aws:accountId}:function:' + AUTH_LAMBDA_NAME,
+          resultTtlInSeconds: 0,
+          identitySource: 'method.request.header.Authorization',
+          type: 'token',
+        }
       },
     },
   ],
